8f643b37bb36548095f0be620c8ec5547e51e0f1
Sergio Suarique <ss_1913@outlook.com>
2024-05-15 15:10:47 -0500

    cors added

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ import { connect } from 'mongoose';
 import { join, dirname } from 'path';
 import { fileURLToPath, pathToFileURL } from 'url';
 import { readdir } from 'fs/promises';
+import cors from 'cors';
 // const CONTRATOS_DB_URI = 'mongodb://user:pass@127.0.0.1:27017/contratos';
 // const CONTRATOS_DB_URI = 'mongodb://127.0.0.1:27017/contratos';
 const CONTRATOS_DB_URI = 'mongodb+srv://srsq02:jlfGEYE2HbC21az7@cluster0.dgha4lq.mongodb.net/contratos?retryWrites=true&w=majority&appName=Cluster0';
 const PORT = process.env.PORT || 3001;
-const BASE_URL = '/api/v1';
 
 const app = express();
 app.use(json());
@@ -16,7 +16,7 @@ const router = express.Router();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const routesDir = join(__dirname, '/routes');
-console.log(routesDir);
+app.use(cors())
 const importRoutes = async () => {
     try {
         const routeFiles = (await readdir(routesDir)).filter(file => file.endsWith('routes.js'));
